d05c49f3cd73aab1a4d4364a2047379572b66c56
Michal Opala <sk4zuzu@gmail.com>
2020-03-26 02:53:16 +0100

    packet/rhe: adding redhat7 image

---
+++ packer/rhe/remote-exec/01-basics.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+set -x
+
+subscription-manager register \
+    --username "$SUBSCRIPTION_USERNAME" \
+    --password "$SUBSCRIPTION_PASSWORD" \
+    --auto-attach \
+    --force
+
+subscription-manager repos \
+    --enable rhel-7-server-rpms \
+    --enable rhel-7-server-extras-rpms \
+    --enable rhel-server-rhscl-7-rpms
+
+yum install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
+
+yum repolist enabled
+
+yum clean all
+
+yum update -y
+
+yum install -y \
+    curl \
+    wget
+
+yum install -y \
+    pv \
+    vim mc htop \
+    net-tools iproute2 netcat nmap \
+    iftop nethogs \
+    jq
+
+yum clean dbcache
+
+install -d -o root -g cloud-user -m ug=rwx,o= /terraform{,/remote-exec}
+
+sync
+
+# vim:ts=4:sw=4:et:syn=sh:
