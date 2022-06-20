3911119416fbdb54145760dc2d86ec50e8d1e510
Michal Opala <sk4zuzu@gmail.com>
2022-06-20 15:48:55 +0200

    packer/kub3lo/rke2: use built-in kubectl

--- packer/kub3lo/rke2/remote-exec/51-rke2.sh
+++ packer/kub3lo/rke2/remote-exec/51-rke2.sh
@@ -9,6 +9,8 @@ set -x
 curl -fsSL "https://github.com/rancher/rke2/releases/download/$RKE2_VERSION/rke2.linux-amd64.tar.gz" \
 | tar -xz -f- -C /usr/local/
 
+ln -s /var/lib/rancher/rke2/bin/kubectl /usr/local/bin/kubectl
+
 install -m u=rwx,go=rx -d /var/lib/rancher/
 install -m u=rwx,go=rx -d /var/lib/rancher/rke2/
 install -m u=rwx,go=   -d /var/lib/rancher/rke2/agent/
