14055cccb332b975f4584601affb5c8545df7e6d
Michal Opala <sk4zuzu@gmail.com>
2022-07-22 16:07:06 +0200

    terraform/debian: add new

---
+++ terraform/debian/nodes/variables.tf
@@ -0,0 +1,51 @@
+variable "env" {
+  type = string
+}
+
+variable "shutdown" {
+  type    = bool
+  default = false
+}
+
+variable "network" {
+  type = object({
+    name    = string
+    domain  = string
+    subnet  = string
+    macaddr = string
+  })
+}
+
+variable "storage" {
+  type = object({
+    pool      = string
+    directory = string
+  })
+}
+
+variable "nodes" {
+  type = object({
+    count   = number
+    prefix  = string
+    offset  = number
+    vcpu    = number
+    memory  = string
+    image   = string
+    storage = string
+    keys    = string
+    disks = list(
+      object({
+        name = string
+        size = string
+      })
+    )
+    mounts = list(
+      object({
+        target = string
+        source = string
+        path   = string
+        ro     = bool
+      })
+    )
+  })
+}
