11cb73043fd6b8f90f68f759aa8a847b57e2fafa
Michal Opala <sk4zuzu@gmail.com>
2022-11-08 20:30:17 +0100

    packer/openbsd: use specific cloud-init 22.3.4

--- packer/openbsd/remote-exec/02-cloud-init.sh
+++ packer/openbsd/remote-exec/02-cloud-init.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env ksh
 
+: "${CLOUD_INIT_RELEASE:=22.3.4}"
+
 set -o errexit -o nounset -o pipefail
 set -x
 
 (install -d /var/tmp/cloud-init/
  cd /var/tmp/cloud-init/
- git clone https://github.com/canonical/cloud-init.git .
+ git clone -b "$CLOUD_INIT_RELEASE" https://github.com/canonical/cloud-init.git .
  ./tools/build-on-openbsd)
 
 cat >'/etc/rc.local' <<'EOF'
