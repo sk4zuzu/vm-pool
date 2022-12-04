9dc3650d937b8f8abc1ca00bb9b2c6999f5319f9
Michal Opala <sk4zuzu@gmail.com>
2022-12-04 19:56:54 +0100

    terraform/almalinux_9p: use default networking (NM)

--- terraform/almalinux_9p/nodes/cloudinit.tf
+++ terraform/almalinux_9p/nodes/cloudinit.tf
@@ -8,6 +8,44 @@ resource "libvirt_cloudinit_disk" "nodes" {
   local-hostname: ${var.nodes.prefix}${count.index + 1}
   EOF
 
+  network_config = <<-EOF
+  version: 2
+  ethernets:
+    eth0:
+      macaddress: '${lower(format(var.network.macaddr, count.index + var.nodes.offset))}'
+      addresses:
+        - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
+      dhcp4: false
+      dhcp6: false
+      gateway4: ${cidrhost(var.network.subnet, 1)}
+      nameservers:
+        addresses:
+          - ${cidrhost(var.network.subnet, 1)}
+        search:
+          - ${var.network.domain}
+    eth1:
+      dhcp4: false
+      dhcp6: false
+    eth2:
+      dhcp4: false
+      dhcp6: false
+  bonds:
+    bond0:
+      interfaces: [eth1, eth2]
+      macaddress: '${lower(format("52:54:56:58:00:%02x", count.index + var.nodes.offset))}'
+      dhcp4: false
+      dhcp6: false
+  vlans:
+    bond0.31:
+      id: 31
+      link: bond0
+      macaddress: '${lower(format("52:54:56:58:31:%02x", count.index + var.nodes.offset))}'
+      addresses:
+        - ${cidrhost("192.168.31.0/24", count.index + var.nodes.offset)}/24
+      dhcp4: false
+      dhcp6: false
+  EOF
+
   user_data = <<-EOF
   #cloud-config
   ssh_pwauth: false
@@ -31,91 +69,10 @@ resource "libvirt_cloudinit_disk" "nodes" {
     - [ ${mount.target}, ${mount.path}, '9p', 'trans=virtio,version=9p2000.L,${mount.ro ? "r" : "rw"}', '0', '0' ]
   %{endfor}
   write_files:
-    - path: /etc/systemd/network/br0.netdev
-      content: |
-        [NetDev]
-        Name=br0
-        Kind=bridge
-    - path: /etc/systemd/network/br0.network
-      content: |
-        [Match]
-        Name=br0
-        [Link]
-        MACAddress=${lower(format(var.network.macaddr, count.index + var.nodes.offset))}
-        ActivationPolicy=always-up
-        [Network]
-        DHCP=no
-        Address=${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
-        Gateway=${cidrhost(var.network.subnet, 1)}
-    - path: /etc/systemd/network/eth0.network
-      content: |
-        [Match]
-        Name=eth0
-        [Link]
-        ActivationPolicy=always-up
-        [Network]
-        Bridge=br0
-
-    - path: /etc/systemd/network/bond0.netdev
-      content: |
-        [NetDev]
-        Name=bond0
-        Kind=bond
-    - path: /etc/systemd/network/bond0.31.netdev
-      content: |
-        [NetDev]
-        Name=bond0.31
-        Kind=vlan
-        [VLAN]
-        Id=31
-    - path: /etc/systemd/network/bond0.31.network
-      content: |
-        [Match]
-        Name=bond0.31
-        [Link]
-        MACAddress=${lower(format("52:54:56:58:31:%02x", count.index + var.nodes.offset))}
-        ActivationPolicy=always-up
-        [Network]
-        DHCP=no
-        Address=${cidrhost("192.168.31.0/24", count.index + var.nodes.offset)}/24
-    - path: /etc/systemd/network/bond0.network
-      content: |
-        [Match]
-        Name=bond0
-        [Link]
-        MACAddress=${lower(format("52:54:56:58:00:%02x", count.index + var.nodes.offset))}
-        ActivationPolicy=always-up
-        [Network]
-        DHCP=no
-        VLAN=bond0.31
-    - path: /etc/systemd/network/eth1.network
-      content: |
-        [Match]
-        Name=eth1
-        [Link]
-        ActivationPolicy=always-up
-        [Network]
-        Bond=bond0
-    - path: /etc/systemd/network/eth2.network
-      content: |
-        [Match]
-        Name=eth2
-        [Link]
-        ActivationPolicy=always-up
-        [Network]
-        Bond=bond0
-
-    - path: /etc/resolv.conf
-      content: |
-        nameserver ${cidrhost(var.network.subnet, 1)}
-        search ${var.network.domain}
     - path: /etc/sysctl.d/98-ip-forward.conf
       content: |
         net.ipv4.ip_forward = 1
-  bootcmd:
-    - systemctl disable NetworkManager --now
   runcmd:
     - sysctl -p /etc/sysctl.d/98-ip-forward.conf
-    - systemctl reload systemd-networkd
   EOF
 }
