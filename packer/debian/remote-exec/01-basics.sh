4d8e7f9456b2e10f641f3daecee666a1124c6e29
Michal Opala <sk4zuzu@gmail.com>
2023-06-07 13:13:24 +0200

    packer/debian: update image (12/20230531-1397)

--- packer/debian/remote-exec/01-basics.sh
+++ packer/debian/remote-exec/01-basics.sh
@@ -27,7 +27,7 @@ apt-get -q install -y --no-install-recommends \
     iftop iproute2 \
     jq \
     mc \
-    net-tools netcat nethogs nmap \
+    net-tools netcat-openbsd nethogs nmap \
     pv \
     vim
 
