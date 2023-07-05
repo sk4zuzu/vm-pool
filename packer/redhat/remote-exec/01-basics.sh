3d27b667a2a21a0df72d1e2cbfac53870d9fe774
Michal Opala <sk4zuzu@gmail.com>
2023-07-05 10:49:15 +0200

    packer/redhat: update image (9.2)

--- packer/redhat/remote-exec/01-basics.sh
+++ packer/redhat/remote-exec/01-basics.sh
@@ -3,6 +3,8 @@
 set -o errexit -o nounset -o pipefail
 set -x
 
+RELEASE=$(awk -F: '/^CPE_NAME=/{print$5}' /etc/os-release)
+
 subscription-manager register \
     --username "$SUBSCRIPTION_USERNAME" \
     --password "$SUBSCRIPTION_PASSWORD" \
@@ -10,9 +12,9 @@ subscription-manager register \
     --force
 
 subscription-manager repos \
-    --enable codeready-builder-for-rhel-8-x86_64-rpms
+    --enable codeready-builder-for-rhel-$RELEASE-x86_64-rpms
 
-dnf install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm"
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-$RELEASE.noarch.rpm
 
 dnf repolist enabled
 
