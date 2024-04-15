40307a0f845e3ea4d80b56c50e701696ba33fd22
Michal Opala <sk4zuzu@gmail.com>
2024-04-15 11:24:33 +0200

    packer/centos: add libvirt-latest repo

--- packer/centos/remote-exec/01-basics.sh
+++ packer/centos/remote-exec/01-basics.sh
@@ -8,6 +8,14 @@ yum install -y centos-release-scl-rh
 yum install -y centos-release-qemu-ev
 yum install -y centos-release-ceph-nautilus
 
+install -o 0 -g 0 -m u=rw,go=r /dev/fd/0 /etc/yum.repos.d/libvirt-latest.repo <<'EOF'
+[libvirt-latest]
+name=CentOS-$releasever - LibVirt-Latest
+baseurl=http://mirror.centos.org/centos/$releasever/virt/$basearch/libvirt-latest/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
 yum install -y \
     ca-certificates curl \
     gpg \
