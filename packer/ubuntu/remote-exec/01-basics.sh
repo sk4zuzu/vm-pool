6e4a43becadba011886c9e7338af9df3504569b1
Michal Opala <sk4zuzu@gmail.com>
2024-04-30 16:37:57 +0200

    packer/ubuntu: update image (24.04/release-20240423)

--- packer/ubuntu/remote-exec/01-basics.sh
+++ packer/ubuntu/remote-exec/01-basics.sh
@@ -8,35 +8,35 @@ export DEBIAN_FRONTEND=noninteractive
 set -o errexit -o nounset -o pipefail
 set -x
 
-gawk -i inplace -f- /etc/cloud/cloud.cfg <<'EOF'
-$1 == "apt_preserve_sources_list:" { $2 = "true"; found=1 }
-{ print }
-END { if (!found) print "apt_preserve_sources_list: true" >> FILENAME }
-EOF
-
-RELEASE=$(lsb_release -sc)
-
-cat >/etc/apt/sources.list <<EOF
-deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE main restricted
-deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-updates main restricted
-deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE universe
-deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-updates universe
-deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-updates multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-backports main restricted universe multiverse
-
-deb http://archive.ubuntu.com/ubuntu $RELEASE main restricted
-deb http://archive.ubuntu.com/ubuntu $RELEASE-updates main restricted
-deb http://archive.ubuntu.com/ubuntu $RELEASE universe
-deb http://archive.ubuntu.com/ubuntu $RELEASE-updates universe
-deb http://archive.ubuntu.com/ubuntu $RELEASE multiverse
-deb http://archive.ubuntu.com/ubuntu $RELEASE-updates multiverse
-deb http://archive.ubuntu.com/ubuntu $RELEASE-backports main restricted universe multiverse
-
-deb http://security.ubuntu.com/ubuntu $RELEASE-security main restricted
-deb http://security.ubuntu.com/ubuntu $RELEASE-security universe
-deb http://security.ubuntu.com/ubuntu $RELEASE-security multiverse
-EOF
+#gawk -i inplace -f- /etc/cloud/cloud.cfg <<'EOF'
+#$1 == "apt_preserve_sources_list:" { $2 = "true"; found=1 }
+#{ print }
+#END { if (!found) print "apt_preserve_sources_list: true" >> FILENAME }
+#EOF
+#
+#RELEASE=$(lsb_release -sc)
+#
+#cat >/etc/apt/sources.list <<EOF
+#deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE main restricted
+#deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-updates main restricted
+#deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE universe
+#deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-updates universe
+#deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE multiverse
+#deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-updates multiverse
+#deb mirror://mirrors.ubuntu.com/mirrors.txt $RELEASE-backports main restricted universe multiverse
+#
+#deb http://archive.ubuntu.com/ubuntu $RELEASE main restricted
+#deb http://archive.ubuntu.com/ubuntu $RELEASE-updates main restricted
+#deb http://archive.ubuntu.com/ubuntu $RELEASE universe
+#deb http://archive.ubuntu.com/ubuntu $RELEASE-updates universe
+#deb http://archive.ubuntu.com/ubuntu $RELEASE multiverse
+#deb http://archive.ubuntu.com/ubuntu $RELEASE-updates multiverse
+#deb http://archive.ubuntu.com/ubuntu $RELEASE-backports main restricted universe multiverse
+#
+#deb http://security.ubuntu.com/ubuntu $RELEASE-security main restricted
+#deb http://security.ubuntu.com/ubuntu $RELEASE-security universe
+#deb http://security.ubuntu.com/ubuntu $RELEASE-security multiverse
+#EOF
 
 apt-get -q update -y
 
@@ -57,7 +57,7 @@ apt-get -q install -y --no-install-recommends \
     iftop iproute2 \
     jq \
     make mc \
-    net-tools netcat nethogs nmap \
+    net-tools netcat-traditional nethogs nmap \
     pv \
     vim
 
