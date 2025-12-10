52fec7dd37c590030997fff46ecfa20de0a71c88
Michal Opala <sk4zuzu@gmail.com>
2025-12-10 21:57:08 +0100

    packer/centos: update (10-20251201.0)

--- packer/centos/remote-exec/01-basics.sh
+++ packer/centos/remote-exec/01-basics.sh
@@ -3,35 +3,23 @@
 set -o errexit -o nounset -o pipefail
 set -x
 
-yum install -y "https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
-yum install -y centos-release-scl-rh
-yum install -y centos-release-qemu-ev
-yum install -y centos-release-ceph-nautilus
+dnf config-manager --set-enabled crb
 
-install -o 0 -g 0 -m u=rw,go=r /dev/fd/0 /etc/yum.repos.d/libvirt-latest.repo <<'EOF'
-[libvirt-latest]
-name=CentOS-$releasever - LibVirt-Latest
-baseurl=http://mirror.centos.org/centos/$releasever/virt/$basearch/libvirt-latest/
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
-EOF
+dnf install -y epel-release
 
-yum install -y \
+dnf install -y \
     ca-certificates curl \
-    gpg \
+    gawk gpg \
     sudo \
     wget
 
-yum install -y \
-    iftop iproute2 \
-    jq \
+dnf install -y \
     htop \
+    iftop iproute \
+    jq \
     mc \
     net-tools netcat nethogs nmap \
-    pv python3 \
+    pciutils pv \
     vim
-
-yum install -y \
-    NetworkManager
 
 sync
