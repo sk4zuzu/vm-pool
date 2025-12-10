141d957467d4d77262db7d28ee39ae6792388de9
Michal Opala <sk4zuzu@gmail.com>
2025-12-10 21:57:08 +0100

    terraform/debian: refactor (0.9.1)

--- terraform/debian/nodes/cloudinit.tf
+++ terraform/debian/nodes/cloudinit.tf
@@ -1,7 +1,12 @@
+locals {
+  pubkeys = flatten([[
+    for v in split("\n", var.nodes.keys) : trimspace(v) if trimspace(v) != ""
+  ]])
+}
+
 resource "libvirt_cloudinit_disk" "nodes" {
   count = var.nodes.count
   name  = "${var.nodes.prefix}${count.index + 1}.iso"
-  pool  = var.storage.pool
 
   meta_data = <<-EOF
   instance-id: ${var.nodes.prefix}${count.index + 1}
@@ -23,20 +28,20 @@ resource "libvirt_cloudinit_disk" "nodes" {
   bridges:
     br0:
       interfaces: [eth0]
-      addresses:
-        - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
       dhcp4: false
       dhcp6: false
       macaddress: '${lower(format(var.network.macaddr, count.index + var.nodes.offset))}'
+      addresses:
+        - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
+      routes:
+        - metric: 0
+          to: 0.0.0.0/0
+          via: ${cidrhost(var.network.subnet, 1)}
       nameservers:
         addresses:
           - ${cidrhost(var.network.subnet, 1)}
         search:
           - ${var.network.domain}
-      routes:
-        - metric: 0
-          to: 0.0.0.0/0
-          via: ${cidrhost(var.network.subnet, 1)}
   EOF
 
   user_data = <<-EOF
@@ -44,13 +49,11 @@ resource "libvirt_cloudinit_disk" "nodes" {
   ssh_pwauth: false
   users:
     - name: debian
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
     - name: root
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
-  chpasswd:
-    list:
-      - debian:asd
-    expire: false
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
   growpart:
     mode: auto
     devices: [/]
@@ -61,13 +64,9 @@ resource "libvirt_cloudinit_disk" "nodes" {
     - [ ${mount.target}, ${mount.path}, '9p', 'trans=virtio,version=9p2000.L,${mount.ro ? "r" : "rw"}', '0', '0' ]
   %{endfor}
   write_files:
-    - content: |
-        nameserver ${cidrhost(var.network.subnet, 1)}
-        search ${var.network.domain}
-      path: /etc/resolv.conf
-    - content: |
+    - path: /etc/sysctl.d/98-ip-forward.conf
+      content: |
         net.ipv4.ip_forward = 1
-      path: /etc/sysctl.d/98-ip-forward.conf
   runcmd:
     - sysctl -p /etc/sysctl.d/98-ip-forward.conf
   EOF
--- terraform/debian/nodes/domain.tf
+++ terraform/debian/nodes/domain.tf
@@ -1,68 +1,114 @@
 resource "libvirt_domain" "nodes" {
-  count  = var.nodes.count
-  name   = "${var.nodes.prefix}${count.index + 1}"
-  vcpu   = var.nodes.vcpu
-  memory = var.nodes.memory
+  count = var.nodes.count
+  name  = "${var.nodes.prefix}${count.index + 1}"
 
-  cloudinit = libvirt_cloudinit_disk.nodes.*.id[count.index]
+  type = "kvm"
 
-  cpu {
-    mode = "host-passthrough"
+  os = {
+    type         = "hvm"
+    type_arch    = "x86_64"
+    type_machine = "q35"
   }
 
-  network_interface {
-    network_name   = var.network.name
-    wait_for_lease = false
-  }
-  dynamic "network_interface" {
-    for_each = toset([
-      "${var.env}none1",
-      "${var.env}none2",
-    ])
-    content {
-      network_name   = network_interface.value
-      wait_for_lease = false
+  features = { acpi = true } # required, otherwise keyboard may not work
+
+  cpu  = { mode = "host-passthrough" }
+  vcpu = var.nodes.vcpu
+
+  memory      = var.nodes.memory
+  memory_unit = "MiB"
+
+  devices = merge(
+    {
+      disks = concat(
+        [
+          {
+            boot   = { order = 1 }
+            driver = { type = "qcow2" } # required, otherwise "raw" driver is used
+            source = {
+              volume = {
+                pool   = var.storage.pool
+                volume = libvirt_volume.nodes_root[count.index].name
+              }
+            }
+            target = {
+              dev = "vda"
+              bus = "virtio"
+            }
+          },
+        ],
+        [
+          for v in var.nodes.disks : {
+            driver = { type = "qcow2" } # required, otherwise "raw" driver is used
+            source = {
+              volume = {
+                pool   = var.storage.pool
+                volume = libvirt_volume.nodes_extra["${var.nodes.prefix}${count.index + 1}-${v.name}"].name
+              }
+            }
+            target = {
+              dev = v.name
+              bus = "virtio"
+            }
+          }
+        ],
+        [
+          {
+            device = "cdrom"
+            driver = { type = "raw" }
+            source = {
+              file = {
+                file = libvirt_volume.nodes_init[count.index].path
+              }
+            }
+            target = {
+              dev = "sdd"
+              bus = "sata"
+            }
+          },
+        ]
+      )
+    },
+    length(var.nodes.mounts) == 0 ? {} : {
+      filesystems = [
+        for v in var.nodes.mounts : {
+          access_mode = "passthrough"
+          target      = { dir = v.target }
+          source      = { mount = { dir = v.source } }
+          read_only   = v.ro
+        }
+      ]
+    },
+    {
+      interfaces = concat(
+        [
+          {
+            type   = "network"
+            model  = { type = "virtio" }
+            source = { network = { network = var.network.name } }
+          },
+        ],
+        [
+          for v in ["${var.env}none1", "${var.env}none2"] : {
+            type   = "network"
+            model  = { type = "virtio" }
+            source = { network = { network = v } }
+          }
+        ]
+      )
+    },
+    {
+      graphics = [
+        {
+          vnc = {
+            listen    = "127.0.0.1"
+            auto_port = true
+          }
+        },
+      ]
     }
-  }
+  )
 
-  console {
-    type        = "pty"
-    target_port = "0"
-    target_type = "serial"
-  }
-
-  console {
-    type        = "pty"
-    target_type = "virtio"
-    target_port = "1"
-  }
-
-  disk {
-    volume_id = libvirt_volume.nodes.*.id[count.index]
-  }
-  dynamic "disk" {
-    for_each = var.nodes.disks
-    content {
-      volume_id = libvirt_volume.nodes_extra["${var.nodes.prefix}${count.index + 1}-${disk.value.name}"].id
-    }
-  }
-
-  dynamic "filesystem" {
-    for_each = var.nodes.mounts
-    content {
-      accessmode = "passthrough"
-      target     = filesystem.value.target
-      source     = filesystem.value.source
-      readonly   = filesystem.value.ro
-    }
-  }
-
-  graphics {
-    type           = "vnc"
-    listen_type    = "address"
-    listen_address = "127.0.0.1"
-    autoport       = true
-  }
-
-  autostart = !var.shutdown
+  running   = var.running
+  autostart = false
 }
