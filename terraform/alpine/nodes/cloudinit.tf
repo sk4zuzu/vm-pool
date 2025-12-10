ab8b021d0057508aacb3f97a3133278f93e058f7
Michal Opala <sk4zuzu@gmail.com>
2025-12-10 21:57:07 +0100

    terraform/alpine: refactor (0.9.1)

--- terraform/alpine/nodes/cloudinit.tf
+++ terraform/alpine/nodes/cloudinit.tf
@@ -1,7 +1,12 @@
+locals {
+  pubkeys = flatten([[
+    for v in split("\n", var.nodes.keys) : trimspace(v) if trimspace(v) != ""
+  ]])
+}
+
 resource "libvirt_cloudinit_disk" "nodes" {
   count = var.nodes.count
   name  = "${var.nodes.prefix}${count.index + 1}.iso"
-  pool  = var.storage.pool
 
   meta_data = <<-EOF
   instance-id: ${var.nodes.prefix}${count.index + 1}
@@ -12,18 +17,26 @@ resource "libvirt_cloudinit_disk" "nodes" {
   version: 2
   ethernets:
     eth0:
-      addresses:
-        - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
       dhcp4: false
       dhcp6: false
-      gateway4: ${cidrhost(var.network.subnet, 1)}
-      macaddress: '${lower(format(var.network.macaddr, count.index + var.nodes.offset))}'
     eth1:
       dhcp4: false
       dhcp6: false
     eth2:
       dhcp4: false
       dhcp6: false
+  bridges:
+    br0:
+      interfaces: [eth0]
+      dhcp4: false
+      dhcp6: false
+      macaddress: '${lower(format(var.network.macaddr, count.index + var.nodes.offset))}'
+      addresses:
+        - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
+      routes:
+        - metric: 0
+          to: 0.0.0.0/0
+          via: ${cidrhost(var.network.subnet, 1)}
   EOF
 
   user_data = <<-EOF
@@ -31,27 +44,28 @@ resource "libvirt_cloudinit_disk" "nodes" {
   ssh_pwauth: false
   users:
     - name: alpine
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
     - name: root
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
-  chpasswd:
-    list:
-      - alpine:asd
-    expire: false
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
   growpart:
     mode: auto
-    devices: ['/']
+    devices: [/]
+  %{if length(var.nodes.mounts) > 0}
+  mounts:
+  %{endif}
+  %{for mount in var.nodes.mounts}
+    - [ ${mount.target}, ${mount.path}, '9p', 'trans=virtio,version=9p2000.L,${mount.ro ? "r" : "rw"}', '0', '0' ]
+  %{endfor}
   write_files:
-    - content: |
-        alpine ALL=(ALL:ALL) NOPASSWD:SETENV: ALL
-      path: /etc/sudoers
-    - content: |
+    - path: /etc/resolv.conf
+      content: |
         nameserver ${cidrhost(var.network.subnet, 1)}
         search ${var.network.domain}
-      path: /etc/resolv.conf
-    - content: |
+    - path: /etc/sysctl.d/98-ip-forward.conf
+      content: |
         net.ipv4.ip_forward = 1
-      path: /etc/sysctl.d/98-ip-forward.conf
   runcmd:
     - sysctl -p /etc/sysctl.d/98-ip-forward.conf
   EOF
