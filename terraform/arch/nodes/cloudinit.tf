b37da53635cd2bc37b7cc87e849629f3ca22345a
Michal Opala <sk4zuzu@gmail.com>
2025-12-10 21:57:08 +0100

    terraform/arch: refactor (0.9.1)

--- terraform/arch/nodes/cloudinit.tf
+++ terraform/arch/nodes/cloudinit.tf
@@ -1,7 +1,12 @@
+locals {
+  pubkeys = flatten([[
+    for v in split("\n", var.nodes.keys) : trimspace(v) if trimspace(v) != ""
+  ]])
+}
+
 resource "libvirt_cloudinit_disk" "nodes" {
   count = var.nodes.count
   name  = "${var.nodes.prefix}${count.index + 1}.iso"
-  pool  = var.storage.pool
 
   meta_data = <<-EOF
   instance-id: ${var.nodes.prefix}${count.index + 1}
@@ -12,12 +17,26 @@ resource "libvirt_cloudinit_disk" "nodes" {
   version: 2
   ethernets:
     eth0:
+      dhcp4: false
+      dhcp6: false
+    eth1:
+      dhcp4: false
+      dhcp6: false
+    eth2:
+      dhcp4: false
+      dhcp6: false
+  bridges:
+    br0:
+      interfaces: [eth0]
+      dhcp4: false
+      dhcp6: false
       macaddress: '${lower(format(var.network.macaddr, count.index + var.nodes.offset))}'
       addresses:
         - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
-      dhcp4: false
-      dhcp6: false
-      gateway4: ${cidrhost(var.network.subnet, 1)}
+      routes:
+        - metric: 0
+          to: 0.0.0.0/0
+          via: ${cidrhost(var.network.subnet, 1)}
       nameservers:
         addresses:
           - ${cidrhost(var.network.subnet, 1)}
@@ -30,17 +49,20 @@ resource "libvirt_cloudinit_disk" "nodes" {
   ssh_pwauth: false
   users:
     - name: arch
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
     - name: root
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
-  chpasswd:
-    list:
-      - arch:asd
-      - root:asd
-    expire: false
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
   growpart:
     mode: auto
     devices: [/]
+  %{if length(var.nodes.mounts) > 0}
+  mounts:
+  %{endif}
+  %{for mount in var.nodes.mounts}
+    - [ ${mount.target}, ${mount.path}, '9p', 'trans=virtio,version=9p2000.L,${mount.ro ? "r" : "rw"}', '0', '0' ]
+  %{endfor}
   write_files:
     - path: /etc/sysctl.d/98-ip-forward.conf
       content: |
