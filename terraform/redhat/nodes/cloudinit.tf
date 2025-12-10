3a41f8fddd4b501393cbf830ca97f05cbbddb4e3
Michal Opala <sk4zuzu@gmail.com>
2025-12-10 21:57:08 +0100

    terraform/redhat: refactor (0.9.1)

--- terraform/redhat/nodes/cloudinit.tf
+++ terraform/redhat/nodes/cloudinit.tf
@@ -1,7 +1,12 @@
+locals {
+  pubkeys = flatten([[
+    for v in split("\n", var.nodes.keys) : trimspace(v) if trimspace(v) != ""
+  ]])
+}
+
 resource "libvirt_cloudinit_disk" "nodes" {
   count = var.nodes.count
   name  = "${var.nodes.prefix}${count.index + 1}.iso"
-  pool  = var.storage.pool
 
   meta_data = <<-EOF
   instance-id: ${var.nodes.prefix}${count.index + 1}
@@ -12,12 +17,26 @@ resource "libvirt_cloudinit_disk" "nodes" {
   version: 2
   ethernets:
     eth0:
-      addresses:
-        - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
       dhcp4: false
       dhcp6: false
-      gateway4: ${cidrhost(var.network.subnet, 1)}
+    eth1:
+      dhcp4: false
+      dhcp6: false
+    eth2:
+      dhcp4: false
+      dhcp6: false
+  bridges:
+    br0:
+      interfaces: [eth0]
+      dhcp4: false
+      dhcp6: false
       macaddress: '${lower(format(var.network.macaddr, count.index + var.nodes.offset))}'
+      addresses:
+        - ${cidrhost(var.network.subnet, count.index + var.nodes.offset)}/${split("/", var.network.subnet)[1]}
+      routes:
+        - metric: 0
+          to: 0.0.0.0/0
+          via: ${cidrhost(var.network.subnet, 1)}
       nameservers:
         addresses:
           - ${cidrhost(var.network.subnet, 1)}
@@ -30,29 +49,24 @@ resource "libvirt_cloudinit_disk" "nodes" {
   ssh_pwauth: false
   users:
     - name: cloud-user
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
     - name: root
-      ssh_authorized_keys: ${jsonencode(var.nodes.keys)}
-  chpasswd:
-    list:
-      - cloud-user:asd
-    expire: false
+      lock_passwd: false
+      ssh_authorized_keys: ${jsonencode(local.pubkeys)}
   growpart:
     mode: auto
     devices: [/]
+  %{if length(var.nodes.mounts) > 0}
+  mounts:
+  %{endif}
+  %{for mount in var.nodes.mounts}
+    - [ ${mount.target}, ${mount.path}, '9p', 'trans=virtio,version=9p2000.L,${mount.ro ? "r" : "rw"}', '0', '0' ]
+  %{endfor}
   write_files:
-    - content: |
-        nameserver ${cidrhost(var.network.subnet, 1)}
-        search ${var.network.domain}
-      path: /etc/resolv.conf
-    - content: |
+    - path: /etc/sysctl.d/98-ip-forward.conf
+      content: |
         net.ipv4.ip_forward = 1
-      path: /etc/sysctl.d/98-ip-forward.conf
-  bootcmd:
-    - sed -i 's|^HWADDR=|MACADDR=|' /etc/sysconfig/network-scripts/ifcfg-eth0
-    - systemctl disable NetworkManager && systemctl stop NetworkManager
-    - systemctl enable network && systemctl start network
-    - ifdown eth0 && ifup eth0
   runcmd:
     - sysctl -p /etc/sysctl.d/98-ip-forward.conf
   EOF
