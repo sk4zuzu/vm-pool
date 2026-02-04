413067543791849ff356fb0157b7b8b7d9d0586a
Michal Opala <sk4zuzu@gmail.com>
2026-02-04 11:08:29 +0100

    packer: add custom packer plugin path / install qemu 1.1.4

--- shell.nix
+++ shell.nix
@@ -5,7 +5,8 @@ with pkgs;
 stdenv.mkDerivation {
   name = "vm-pool-env";
   buildInputs = [
-    cdrkit cloud-utils curl
+    cdrkit cloud-utils coreutils curl
+    jq
     git
     libxslt
     unzip
