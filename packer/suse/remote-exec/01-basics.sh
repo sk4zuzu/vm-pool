43a44932312c3686f5cdd8024c2b2b76cd1dd63f
Michal Opala <sk4zuzu@gmail.com>
2026-03-24 15:50:06 +0100

    packer/suse: add more repos and packages

--- packer/suse/remote-exec/01-basics.sh
+++ packer/suse/remote-exec/01-basics.sh
@@ -5,18 +5,20 @@ set -x
 
 SUSEConnect -r "$REGISTRATION_CODE"
 SUSEConnect -p "sle-module-legacy/$VERSION/x86_64"
+SUSEConnect -p "sle-module-desktop-applications/$VERSION/x86_64"
+SUSEConnect -p "sle-module-development-tools/$VERSION/x86_64"
 SUSEConnect -p "PackageHub/$VERSION/x86_64"
 
 zypper --non-interactive --gpg-auto-import-keys refresh
 
 zypper --non-interactive install -y \
     bridge-utils \
-    gawk \
+    gawk git \
     htop \
     iftop iproute2 \
     jq \
     make mc \
-    net-tools netcat nethogs nmap \
+    net-tools netcat nethogs nftables nmap \
     pv \
     vim
 
